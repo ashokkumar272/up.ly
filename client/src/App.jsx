40b5d74044a9a5391957b380cf0f0e62d8def91b
Maheep Tulsian <maheep_tulsian@srmap.edu.in>
2025-03-16 01:29:53 +0530

    To aaiye shuru karte hai!!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './components/ThemeProvider';
-import LandingPage from './pages/LandingPage';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import ForgotPassword from './pages/ForgotPassword';
-import Dashboard from './pages/Dashboard';
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route path="/dashboard" element={<Dashboard />} >
-          </Route>
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  );
-};
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
 
-export default App;
+export default App
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
-import path from "path"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    tailwindcss(),
+  ],
 })
